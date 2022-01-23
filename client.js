b1071d78c57423ab77428b021c89d27e0cd2c7bb
Navnath <68325294+NavnathShinde2104@users.noreply.github.com>
2022-01-24 00:15:14 +0530

    Add files via upload

--- client.js
+++ client.js
@@ -7,6 +7,12 @@ const messageInp = document.getElementById('messageInp');
 
 const messsageContainer = document.querySelector('.container');
 
+var join_chat_notification = new Audio('join_chat_notification.mp4');
+
+var new_msg_notification = new Audio('new_msg_notification.mp4');
+
+var left_chat_notification = new Audio('left_chat_notification.mp4');
+
 const append = (message , position) => {
 
     const messageElement = document.createElement('div');
@@ -15,11 +21,16 @@ const append = (message , position) => {
     messageElement.classList.add(position);
     messsageContainer.append(messageElement);
 
+
 }
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const message = messageInp.value;
+    if(`${message}`==``){
+        window.alert(`no msg! plz type something`)
+        return false
+    }
     append(`You: ${message}`,'right');
     socket.emit('send',message);
     messageInp.value='';
@@ -28,14 +39,26 @@ form.addEventListener('submit',(e)=>{
 
 const cname = prompt("Enter your name to join");
 
+
+
 socket.emit('new-user-joined',cname);
 
 socket.on('user-joined',cname =>{
+    join_chat_notification.play();
     append(`${cname} joined the chat`,'right')
+    
+
 
 })
 
 socket.on('receive',data =>{
     append(`${data.cname}: ${data.message}`,'left')
+    new_msg_notification.play();
+
+})
+
+socket.on('left',cname =>{
+    append(`${cname}: left the chat`,'left')
+    left_chat_notification.play();
 
 })
